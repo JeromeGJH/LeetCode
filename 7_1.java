a2deb8e2dafec245ced0a3ed20b60e498d436a2f
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-06-03 18:35:49 +0800

    Create 7_1.java

---
+++ 7_1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+        int res = 0;
+        while(x != 0){
+            int tmp = x % 10;
+            if(res < -214748364 || (res == 214748364 && tmp < -8)){return 0;}
+            if(res > 214748364 || (res == 214748364 && tmp > 7)){return 0;}
+            res = res * 10 + tmp;
+            x /= 10;
+        }
+        return res;
+    }
+}
