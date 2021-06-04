1cc13264784a2b88143b0c337734f9079a295a27
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-06-04 20:39:43 +0800

    Create 14_1.cpp

---
+++ 14_1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return string();
+        sort(strs.begin(), strs.end());
+        string st = strs.front(), en = strs.back();
+        int i, num = min(st.size(), en.size());
+        for(i = 0; i < num && st[i] == en[i]; i ++);
+        return string(st, 0, i);
+    }
+};
