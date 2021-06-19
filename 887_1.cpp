8ebf967528d4c6ab57184aacd4728914e98c4328
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-06-20 06:15:50 +0800

    Create 887_1.cpp

---
+++ 887_1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int calcF(int K, int T){
+        if( T == 1 || K == 1){
+            return T + 1;
+        }else{
+            return calcF(K-1, T-1) + calcF(K, T-1);
+        }
+    
+    }
+    int superEggDrop(int k, int N) {
+        int T = 1;
+        while(calcF(k ,T) < N + 1){
+            T++;
+        }
+        return T;
+
+    }
+};
