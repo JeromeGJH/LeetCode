d204fb78643c91389341a71498033482a09b253a
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-06-23 19:36:42 +0800

    Create 403_1.cpp

---
+++ 403_1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canCross(vector<int>& stones) {
+        int l = stones.size();
+        if(l == 2 && stones[1] - stones[0] == 1){
+            return true;
+        }
+        vector<set<int>> dp(l, set<int>());
+        dp[0].insert(1);
+        if(stones[1] - stones[0] == 1){
+            dp[1].insert(1);
+        }else{
+            return false;
+        }
+        int diff = stones[2] - stones[1];
+        if(diff == 1 || diff == 2){
+            dp[2].insert(diff);
+        }else{
+            return false;
+        }
+        if(l == 3){
+            return true;
+        }
+        int i = 0, j  = 0, k = 0;
+        for(i = 3; i< l; ++i){
+            for(j = i-1; j >= 0; --j){
+                diff = stones[i] - stones[j];
+                if(dp[j].empty() || *(--dp[j].end()) +1 < diff){
+                    continue;
+                }
+                for(set<int>::iterator it = dp[j].begin(); it != dp[j].end(); it++){
+                    if(*it - 1 <= diff && diff <= *it + 1)
+                        dp[i].insert(diff);
+                }
+            }
+        }
+        return !dp[l-1].empty();
+    }
+};
