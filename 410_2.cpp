9161fe6cc22143dcb075ee2bf65e39d022cc52c3
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-06-24 19:21:47 +0800

    Create 410_2.cpp
    二分查找

---
+++ 410_2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool check(vector<int>& nums, int x, int m) {
+        long long sum = 0;
+        int cnt = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (sum + nums[i] > x) {
+                cnt++;
+                sum = nums[i];
+            } else {
+                sum += nums[i];
+            }
+        }
+        return cnt <= m;
+    }
+
+    int splitArray(vector<int>& nums, int m) {
+        long long left = 0, right = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            right += nums[i];
+            if (left < nums[i]) {
+                left = nums[i];
+            }
+        }
+        while (left < right) {
+            long long mid = (left + right) >> 1;
+            if (check(nums, mid, m)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
+
