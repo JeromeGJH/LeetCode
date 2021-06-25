610541596964b05f1c4e4eed6539b20329144700
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-06-26 02:50:00 +0800

    Create 188_1.cpp

---
+++ 188_1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int l  = prices.size();
+        if( k == 0 || l == 0){
+            return 0;
+        }
+        vector<int> sell(k , 0);
+        vector<int> buy(k, 0);
+        for(int i = 0; i < k; ++i){
+            buy[i] = -prices[0];
+        }
+        for(int i = 1; i < l; ++i){
+            for(int j = 0; j < k; ++j){
+                if(j == 0){
+                    buy[j] = max(buy[j], -prices[i]);
+                    sell[j] = max(sell[j], prices[i] + buy[j]);
+                }else{
+                    buy[j] = max(buy[j], sell[j-1] - prices[i]);
+                    sell[j] = max(sell[j], prices[i] + buy[j]);
+                }
+            }
+        }
+        return sell[k-1];
+
+    }
+};
