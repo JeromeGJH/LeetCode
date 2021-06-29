a524188e391fbb0a80bd5786a7ba28a29ca2d578
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-06-30 02:10:15 +0800

    Create 10_1.cpp

---
+++ 10_1.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int l1 = s.length(), l2 =p.length();
+        if(l2 == 0){
+            if(l1 == 0){
+                return true;
+            }
+            return false;
+        }
+        if(l1 == 0){
+            if(l2 == 1){
+                return false;
+            }
+        }
+        int i = 0 , j = 0, k = 0;
+        vector<vector<int>> dp(l1+1, vector<int>(l2+1, 0));
+        dp[0][0] = 1;
+        for(j = 1; j <= l2; ++j){
+            if(p[j-1] == '.'){
+                for(i = 1; i <= l1; ++i){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+            }else if(p[j-1] == '*'){
+                if(j != 1){
+                    int flag = 0;
+                    for(i = 0; i <= l1; ++i){
+                        if(dp[i][j-2] == 1){
+                            dp[i][j] = 1;
+                            for(k = i+1; k <= l1; ++k){
+                                if(s[k-1] == p[j-2] || p[j-2] == '.'){
+                                    dp[k][j] = 1;
+                                }else{
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }else{
+                for(i = 1; i <= l1; ++i){
+                    if(s[i-1] == p[j-1]){
+                        dp[i][j] = dp[i-1][j-1];
+                    }
+                }
+            }
+        }
+        // for( j = 0 ; j <= l2; ++j){
+        //     for( i = 0 ; i <= l1; ++i){
+        //         cout <<"i: "<<i <<" j: "<<j << "   "<<dp[i][j] << endl;
+        //     }
+        // }
+        return dp[l1][l2] == 1;
+    }
+};
