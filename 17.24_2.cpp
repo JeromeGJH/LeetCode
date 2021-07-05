816032643c93e56a1a540ed398d973bb9c2289b9
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-07-06 04:07:17 +0800

    Update 17.24_2.cpp

--- 17.24_2.cpp
+++ 17.24_2.cpp
@@ -2,52 +2,34 @@ class Solution {
 public:
     vector<int> getMaxMatrix(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        int i = 0, j = 0, k = 0, ans = matrix[0][0], l = 0, a = 0;
+        int i = 0, j = 0, k = 0, ans = matrix[0][0];
         vector<int> ret(4, 0);
         for( i = 0; i < m; ++i){
             vector<int> sum(n, 0);
             for(j = i; j < m ;++j){
-                vector<int> s(n, 0);
+                int start, value;
                 for(k = 0; k < n; ++k){
                     sum[k] += matrix[j][k];
                     if(k == 0){
-                        s[0] = sum[0];
+                        value = sum[0]>=0?sum[0]:0;
+                        start = sum[0]>=0?0:1;
                     }else{
-                        s[k] = s[k-1] + sum[k];
+                        value += sum[k];
+                        if(value > ans){
+                            ans = value;
+                            ret[0] = i;
+                            ret[1] = start;
+                            ret[2] = j;
+                            ret[3] = k;
+                        }
+                        if(value < 0){
+                            value = 0;
+                            start = k+1;
+                        }
                     }
                 }
-                int start, value;
-                if(s[0] >= 0){
-                    value = s[0];
-                    start = 0;
-                }else{
-                    value = 0;
-                    start = 1;
-                }
-                for( a = 1; a < n; ++a){
-                    value += sum[a];
-                    if(value > ans){
-                        // cout<<"here1";
-                        // cout << j;
-                        ans = value;
-                        ret[0] = i;
-                        ret[1] = start;
-                        ret[2] = j;
-                        ret[3] = a;
-                    }
-                    if(value < 0){
-                        value = 0;
-                        start = a+1;
-                    }
-                }
-                 
-                // if(j == 0){
-                //     cout << sum[0]<<"  "<<sum[1];
-                // }
             }
         }
         return ret;
-
-
     }
 };
