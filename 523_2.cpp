ae1f5bd3fa6932ea6beac57c07862747429af408
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-07-13 12:12:28 +0800

    Create 523_2.cpp

---
+++ 523_2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int m = nums.size();
+        if (m < 2) {
+            return false;
+        }
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        int remainder = 0;
+        for (int i = 0; i < m; i++) {
+            remainder = (remainder + nums[i]) % k;
+            if (mp.count(remainder)) {
+                if (i - mp[remainder] >= 2) {
+                    return true;
+                }
+            } else {
+                mp[remainder] = i;
+            }
+        }
+        return false;
+    }
+};
