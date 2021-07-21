dc211416bb1fb0e35aedcf162858703c3b857efe
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-07-22 00:51:06 +0800

    Create 1147_1.cpp

---
+++ 1147_1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int ifEqual(string s, int l){
+        if(l <= 1){
+            return l;
+        }
+        int i = 1, ans = 0, flag = 0;
+        for(i = 1; i <= l/2; ++i){
+            if(s.substr(0, i) == s.substr(l-i, i)){
+                flag = 1;
+                ans = max(ans, 2 + ifEqual(s.substr(i, l - i*2), l - i*2));
+            }
+        }
+        return flag == 0? 1:ans;
+    }
+
+    int longestDecomposition(string text) {
+        int l = text.length();
+        return ifEqual(text, l);
+    }
+};
