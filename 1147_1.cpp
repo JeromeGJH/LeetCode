8f52c806b3ee76f18f2f6cd8b03abdaa41ff9777
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-07-22 01:07:09 +0800

    Hard

--- 1147_1.cpp
+++ 1147_1.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int ifEqual(string s, int l){
-        if(l <= 1){
-            return l;
-        }
-        int i = 1, ans = 0, flag = 0;
-        for(i = 1; i <= l/2; ++i){
+        for(int i = 1; i <= l/2; ++i){
             if(s.substr(0, i) == s.substr(l-i, i)){
-                flag = 1;
-                ans = max(ans, 2 + ifEqual(s.substr(i, l - i*2), l - i*2));
-                break;
+                return 2 + ifEqual(s.substr(i, l - i*2), l - i*2);
             }
         }
-        return flag == 0? 1:ans;
+        return l>0 ? 1:0;
     }
 
     int longestDecomposition(string text) {
