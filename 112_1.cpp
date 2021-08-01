72ff50e5061c78a12f21fc583f66fee06edee5d7
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-08-02 02:21:20 +0800

    Create 112_1.cpp

---
+++ 112_1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == nullptr){
+            return false;
+        }
+        targetSum -= root->val;
+        if(targetSum== 0 && root->left == nullptr && root->right == nullptr){
+            return true;
+        }
+        return (hasPathSum(root->left, targetSum) || !hasPathSum(root->right, targetSum));
+    }
+};
