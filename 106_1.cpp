a88f5699b5b482cfa68aafccc548dc848e50ae9f
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2021-08-08 00:14:10 +0800

    Create 106_1.cpp

---
+++ 106_1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if (postorder.size() == 0) {
+            return nullptr;
+        }
+        auto root = new TreeNode(postorder[postorder.size() - 1]);
+        auto s = stack<TreeNode*>();
+        s.push(root);
+        int inorderIndex = inorder.size() - 1;
+        for (int i = int(postorder.size()) - 2; i >= 0; i--) {
+            int postorderVal = postorder[i];
+            auto node = s.top();
+            if (node->val != inorder[inorderIndex]) {
+                node->right = new TreeNode(postorderVal);
+                s.push(node->right);
+            } else {
+                while (!s.empty() && s.top()->val == inorder[inorderIndex]) {
+                    node = s.top();
+                    s.pop();
+                    inorderIndex--;
+                }
+                node->left = new TreeNode(postorderVal);
+                s.push(node->left);
+            }
+        }
+        return root;
+    }
+};
