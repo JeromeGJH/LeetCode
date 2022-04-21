cff29b19deabb7f515e96b7cd0d9731c64308e0a
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2022-04-21 12:28:36 +0800

    Easy
    Hash Table

---
+++ 350_2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return intersect(nums2, nums1);
+        }
+        unordered_map <int, int> m;
+        for (int num : nums1) {
+            ++m[num];
+        }
+        vector<int> intersection;
+        for (int num : nums2) {
+            if (m.count(num)) {
+                intersection.push_back(num);
+                --m[num];
+                if (m[num] == 0) {
+                    m.erase(num);
+                }
+            }
+        }
+        return intersection;
+    }
+};
