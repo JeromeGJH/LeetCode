125fd11b04d961ec7499efffc24b29a85faf0f65
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2022-04-21 17:30:22 +0800

    Median
    Binary Search

---
+++ 0153_2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1, mid;
+        while(left < right){
+            mid = (left + right)/2;
+            if(nums[mid] < nums[right]){
+                right = mid;
+            }else{
+                left = mid+1;
+            }
+        }
+        return nums[left];
+    }
+};
