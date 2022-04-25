e93ebd20d0171def288d87f68185683b35a9b678
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2022-04-25 18:32:27 +0800

    Median
    DP

--- 1552_2.cpp
+++ 1552_2.cpp
@@ -3,31 +3,16 @@ public:
     int maxDistance(vector<int>& position, int m) {
         sort(position.begin(), position.end());
         int n = position.size();
-        int i = 0, j = 0, k,dist, tmp;
+        int i = 0, j = 0, k;
         vector<vector<int>> dp(n, vector<int>(m + 1, 0));
         for(i = 1; i < n; ++i){
             dp[i][2] = position[i] - position[0];
             for(j = 3; j <= m; ++j){
                 for(k = i - 1; k >= j - 2; --k){
-                    dist = position[i] - position[k];
-                    if(dist > dp[k][j-1]){
-                        tmp = dp[k][j-1];
-                    }else{
-                        tmp = dist;
-                    }
-                    // if(i == 2 && k == 1){
-                    //     cout << dist << " " << tmp << endl;
-                    // }
-                    dp[i][j] = max(dp[i][j], tmp);
+                    dp[i][j] = max(dp[i][j], min(position[i] - position[k], dp[k][j-1]));
                 }
             }
         }
-        // for(i = 1; i < n; ++i){
-            
-        //     for(j = 2; j <= m; ++j){
-        //         cout << i << " " << j << " " << dp[i][j] << endl;;
-        //     }
-        // }
         return dp[n-1][m];
     }
 };
