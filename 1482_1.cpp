0181be389ed2fd5dcde6ded6b8b1cc405ee01cf0
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2022-04-27 14:54:47 +0800

    Median
    Binary Search

--- 1482_1.cpp
+++ 1482_1.cpp
@@ -2,18 +2,12 @@ class Solution {
 public:
     auto check(int day, int m, int k, vector<int>& bloomDay){
         int i, j, count = 0, flw = 0, n = bloomDay.size();
-        vector<bool> state(n+1, false);
         for(i = 0; i < n; ++i){
-            if(day >= bloomDay[i]){
-                state[i] = true;
-            }
-        }
-        for(i = 0; i < n; ++i){
-            while(state[i]){
+            while(i < n && day >= bloomDay[i]){
                 ++flw;
                 ++i;
             }
-            
+
             if(flw > 0){
                 count += flw / k;
                 flw = 0;
