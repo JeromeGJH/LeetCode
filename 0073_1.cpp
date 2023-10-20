c632d8ff7e6d196d363c82b942ad49348e320c5c
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2023-10-20 16:49:36 +0800

    Median

--- 0073_1.cpp
+++ 0073_1.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_map<int, bool> zeros;
         int row = matrix.size();
         int col = matrix[0].size();
+        vector<int> zeros(row+col, 0);
+        
         int i, j;
         for(i = 0; i < row; ++i){
             for(j = 0; j< col; ++j){
                 if(matrix[i][j] == 0){
-                    zeros[i] = true;
-                    zeros[row+j] = true;
+                    zeros[i] = 1;
+                    zeros[row+j] = 1;
                 }
             }
         }
 
         for(i = 0; i < row; ++i){
-            if(zeros.count(i) == 1){
+            if(zeros[i] == 1){
                 for(j = 0; j < col; ++j){
                     matrix[i][j] = 0;
                 }
             }
         }
         for(j = 0; j < col; ++j){
-            if(zeros.count(j+row) == 1){
+            if(zeros[row+j] == 1){
                 for(i = 0; i < row; ++i){
                     matrix[i][j] = 0;
                 }
