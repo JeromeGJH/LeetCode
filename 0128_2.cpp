9ad329073cc0574e5254a6c8937f1e96b88dce89
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2023-10-21 17:31:46 +0800

    Median

---
+++ 0128_2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int len = nums.size();
+        if(len <= 1){
+            return len;
+        }
+        int i = 0;
+        unordered_map<int, bool> count;
+        for(i = 0; i < len; ++i){
+            if(count.count(nums[i]) == 0){
+                count[nums[i]] = true;
+            }
+        }
+        int res = 1;
+
+        for(auto it = count.begin(); it != count.end(); ++it){
+            int num = it->first;
+            if(count.count(num-1) == 0 && count.count(num+res) > 0){
+                int l = 0;
+                while(count.count(num) == 1){
+                    l += 1;
+                    ++num;
+                }
+                res = max(res, l);
+            }
+
+            
+        }
+        return res;
+
+
+    }
+};
