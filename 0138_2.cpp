a2d8231eced7f53dabc8b9b8b01ad73012dbff1a
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2023-10-21 23:05:09 +0800

    median

---
+++ 0138_2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    unordered_map<Node*, Node*> cachedNode;
+
+    Node* copyRandomList(Node* head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        if (!cachedNode.count(head)) {
+            Node* headNew = new Node(head->val);
+            cachedNode[head] = headNew;
+            headNew->next = copyRandomList(head->next);
+            headNew->random = copyRandomList(head->random);
+        }
+        return cachedNode[head];
+    }
+};
