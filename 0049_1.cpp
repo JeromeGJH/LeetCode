fe37e4a37febe904ca0a063314e377daf7949fde
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2023-10-21 16:22:04 +0800

    median

---
+++ 0049_1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        int len = strs.size();
+        if(len <= 1){
+            ans.emplace_back(strs);
+            return ans;
+        }
+
+        unordered_map<string, vector<int>> word;
+        int i, j;
+        for(i = 0; i< len; ++i){
+            string s = "";
+            vector<int> count(26, 0);
+            for(j = 0; j < strs[i].length(); ++j){
+                int index = strs[i][j] - 'a';
+                count[index] += 1;
+            }
+            for(k = 0; k < 26; ++k){
+                s.append(to_string(count[k]));
+                s.append("0");
+            }
+            cout << s << endl;
+            if(word.count(s) == 0){
+                word[s] = vector<int>({i});
+            }else{
+                word[s].emplace_back(i);
+            }
+        }
+        for(auto it = word.begin(); it != word.end(); ++it){
+            vector<string> vs;
+            int size = it->second.size();
+            for(i = 0; i < size; ++i){
+                vs.emplace_back(strs[it->second[i]]);
+            }
+            ans.emplace_back(vs);
+            vs.clear();
+        }
+        return ans;
+    }
+};
