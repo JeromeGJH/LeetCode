49eb536258e179c1bbac362dc4d1a776722443ba
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2023-10-23 02:58:14 +0800

    Median

--- 0103_1.cpp
+++ 0103_1.cpp
@@ -1,7 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> res;
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+                vector<vector<int>> res;
         if(root == nullptr){
             return res;
         }
@@ -9,49 +20,32 @@ public:
         vector<TreeNode*> nex;
         vector<int> nums;
         cur.emplace_back(root);
-        int i = 0;
-        bool flag = true;
+        int i = 0, j;
+        bool flag = 1;
         while(true){
             if(cur.empty()){
                 break;
             }
-            if(flag){
-                nums.emplace_back(cur[i]->val);
-                if(cur[i]->left){
-                    nex.emplace_back(cur[i]->left);
-                }
-                if(cur[i]->right){
-                    nex.emplace_back(cur[i]->right);
-                }
-                if(i == cur.size()-1){
-                    res.emplace_back(nums);
-                    nums = vector<int>();
-                    cur = nex;
-                    nex = vector<TreeNode*>();
-                    i = cur.size()-1;
-                    flag = false;
-                    continue;
-                }
-                ++i;
-            }else{
-                nums.emplace_back(cur[i]->val);
-                if(cur[i]->left){
-                    nex.emplace_back(cur[i]->left);
-                }
-                if(cur[i]->right){
-                    nex.emplace_back(cur[i]->right);
-                }
-                if(i == 0){
-                    res.emplace_back(nums);
-                    nums = vector<int>();
-                    cur = nex;
-                    nex = vector<TreeNode*>();
-                    i = 0;
-                    flag = true;
-                    continue;
-                }
-                --i;
+            nums.emplace_back(cur[i]->val);
+            if(cur[i]->left){
+                nex.emplace_back(cur[i]->left);
             }
+            if(cur[i]->right){
+                nex.emplace_back(cur[i]->right);
+            }
+            if(i == cur.size()-1){
+                if(flag == 0){
+                    nums = vector<int>(nums.rbegin(), nums.rend());
+                }
+                res.emplace_back(nums);
+                nums = vector<int>();
+                cur = nex;
+                nex = vector<TreeNode*>();
+                i = 0;
+                flag = 1 - flag;;
+                continue;
+            }
+            ++i;
         }
         return res;
     }
