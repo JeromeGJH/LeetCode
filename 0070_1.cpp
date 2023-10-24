4aca8189c3460d373e3dc2ef66155606c935d75a
JeromeGJH <48841611+JeromeGJH@users.noreply.github.com>
2023-10-24 20:57:45 +0800

    Median

---
+++ 0070_1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1){
+            return 1;
+        }
+        vector<int> dp(n, 1);
+        dp[1] = 2;
+        int i = 0;
+        for(i = 2; i < n; ++i){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
+    }
+};
